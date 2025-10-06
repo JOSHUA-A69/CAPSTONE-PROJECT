ce935c454b3aa99289be12114e909b5c08780793
JOSHUA-A69 <ujoshua368@gmail.com>
2025-10-06 22:35:39 +0800

    minor adjustments

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen flex flex-col bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
@@ -30,9 +30,10 @@
             @endisset
 
             <!-- Page Content -->
-            <main>
+            <main class="flex-1">
                 {{ $slot }}
             </main>
+
             @include('layouts.footer')
         </div>
     </body>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,11 +17,14 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 pb-12 bg-gray-100 dark:bg-gray-900">
-            <div class="w-full sm:max-w-md mt-6 px-4 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+        <div class="min-h-screen flex flex-col bg-gray-100 dark:bg-gray-900">
+            <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 pb-12">
+                <div class="w-full sm:max-w-md mt-6 px-4 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+                    {{ $slot }}
+                </div>
             </div>
+
+            @include('layouts.footer')
         </div>
-        @include('layouts.footer')
     </body>
 </html>
