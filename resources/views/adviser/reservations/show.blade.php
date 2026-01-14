15d6f3cc6c1a10cb05530099d55731760793cf3b
JOSHUA-A69 <ujoshua368@gmail.com>
2026-01-14 21:26:13 +0800

    small refinement

--- resources/views/adviser/reservations/show.blade.php
+++ resources/views/adviser/reservations/show.blade.php
@@ -308,8 +308,8 @@
 </div>
 
 <!-- Reject Modal -->
-<div id="rejectModal" class="fixed inset-0 bg-gray-900 bg-opacity-50 hidden z-50">
-    <div class="bg-white dark:bg-gray-800 rounded-lg shadow-xl w-full max-w-md mx-4">
+<div id="rejectModal" class="fixed inset-0 bg-gray-900 bg-opacity-50 hidden z-[100] items-center justify-center">
+    <div class="bg-white dark:bg-gray-800 rounded-lg shadow-xl w-full max-w-lg mx-4">
         <div class="px-6 py-4 border-b dark:border-gray-700">
             <h3 class="text-xl font-semibold text-gray-900 dark:text-white">Reject Reservation</h3>
         </div>
